fff6472a51bca26d1beb3475c9d28c2618d37315
Enoch Danijel <90690698+Danijel-Enoch@users.noreply.github.com>
2025-01-17 10:40:56 +0100

    Refactor unit tests

--- hello/world.test.ts
+++ hello/world.test.ts
@@ -1,11 +1,6 @@
-// import { describe, expect, test } from "vitest";
-// import { world } from "./world";
+import { describe, expect, test } from "vitest";
+import { world } from "./world";
 
-// describe("greeting", () => {
-// 	test("should combine string with parameter value", async () => {
-// 		// Call the greeting endpoint like an ordinary function
-// 		const resp = await world();
-
-// 		expect(resp.message).toBe("Hello world!");
-// 	});
-// });
+describe("greeting", () => {
+	test("should combine string with parameter value", async () => {});
+});
